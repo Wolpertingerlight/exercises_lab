bb0c449aea12f08841ad113bcf219bfe2a5aba46
lavarius-dev <okhrimenko.yura99@gmail.com>
2022-11-19 19:23:58 +0500

    Умножение

--- src/Exercise_1/Test1.java
+++ src/Exercise_1/Test1.java
@@ -9,22 +9,20 @@ public class Test1 {
         a.increment(); // 1 операция
         a.increment(); // 2 операция
         int count = 2;
-        while (a.getValue() <= 1000){
+        while (a.getValue() <= 1000) {
             a = a.add(a);
             count++;
         }
-        while (a.getValue() != 1000){
+        while (a.getValue() != 1000) {
             a.decrement();
             count++;
         }
         System.out.println("Значение а: " + a.getValue());
         System.out.println("Всего операций " + count);
-//        for (int i = 0; i < 49; i++) {
-//            if (a.getValue() <= 1000){
-//                count++;
-//                a = a.add(a);
-//            }
-//            System.out.println(a.getValue());
-//        }
+        Int first = new Int(-1);
+        Int second = new Int(-1);
+        System.out.println("Произведение (" + first + ") x (" + second + ") = " + first.multiply(second));
+        second = new Int(-3);
+        System.out.println("Произведение (" + first + ") x (" + second + ") = " + first.multiply(second));
     }
 }
